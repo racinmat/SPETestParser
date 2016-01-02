f7d0d59f42426186e6d0247147d0b310c4ef19ea
Azathoth <memnarch@seznam.cz>
2016-01-02 21:11:48 +0100

    přechod z polí na objekty, kvůli přehlednosti, ale hlavně kvůli předávání referencí

--- tests/acceptance/downloaderCept.php
+++ tests/acceptance/downloaderCept.php
@@ -2,7 +2,61 @@
 /** @var \Codeception\TestCase\Cept $this */	//typehint na proměnnou $this
 /** @var \Codeception\Scenario $scenario */	//typehint na proměnnou $this
 
+class Question {
+	/** @var Answer[] */
+	public $answers;
+	/** @var Answer */
+	public $correctAnswer;
+	/** @var Answer */
+	public $selected;
+	/** @var string */
+	public $text;
 
+	/**
+	 * Question constructor.
+	 */
+	public function __construct($text)
+	{
+		$this->correctAnswer = null;
+		$this->answers = [];
+		$this->text = $text;
+		$this->selected = null;
+	}
+
+	public function hasCorrectAnswer()
+	{
+		return $this->correctAnswer != null;
+	}
+}
+
+class Answer {
+	/** @var string */
+	public $text;
+	/** @var bool */
+	public $correct;
+	/** @var int */
+	public $id;
+	/** @var bool */
+	public $tried;
+
+	/**
+	 * Answer constructor.
+	 * @param string $text
+	 * @param bool $correct
+	 * @param int $id
+	 * @param bool $tried
+	 */
+	public function __construct($text, $correct, $id, $tried)
+	{
+		$this->text = $text;
+		$this->correct = $correct;
+		$this->id = $id;
+		$this->tried = $tried;
+	}
+
+}
+
+/** @var Question[] $allTests */
 $allTests = json_decode(file_get_contents('tests.json'), true);
 $currentTest = []; //question => correct answer
 
@@ -15,40 +69,43 @@ $I->see('testu stiskem');
 
 $array = [];
 for ($i = 1; $i <= 30; $i++) {
-	$question = $I->grabTextFrom("~$i.<\/b> otázka \(.*?\) - +<b>(.*?)<\/b>~");
-	$currentTest[] = $question;
-	if (array_key_exists($question, $allTests)) {
-		$answers = $allTests[$question];
+	$questionText = $I->grabTextFrom("~$i.<\/b> otázka \(.*?\) - +<b>(.*?)<\/b>~");
+	$currentTest[] = $questionText;
+	if (array_key_exists($questionText, $allTests)) {
+		$question = $allTests[$questionText];
 	} else {
-		$answers = [];
+		/** @var Answer[] $question */
+		$question = [];
 		$answer1number = $I->grabTextFrom("~<TD VALIGN=\"MIDDLE\"><INPUT TYPE=\"radio\" VALUE=\"(\d+)\" NAME=\"a$i\" \/><\/TD><TD>(.*?)<BR /></TD>~i");
 		$answersCount = $I->getNumberOfElements("input[name=a$i]");
 		for ($j = 0; $j < $answersCount; $j++) {
 			$number = $answer1number + $j;
 			$answerText = $I->grabTextFrom("~<TD VALIGN=\"MIDDLE\"><INPUT TYPE=\"radio\" VALUE=\"$number\" NAME=\"a$i\" \/><\/TD><TD>(.*?)<BR /></TD>~i");
-			$answers[] = ['text' => $answerText, 'tried' => false, 'correct' => null, 'id' => $number, 'selected' => false];
+			$question[] = new Answer($answerText, null, $number, false);
 		}
-		$allTests[$question] = ['answers' => $answers, 'hasCorrectAnswer' => false];
+		$allTests[$questionText] = new Question($questionText);
 	}
 }
 
 //naklikání nevyzkoušených odpovědí
 $i = 0;
-foreach ($currentTest as $question) {
-	$answers = $allTests[$question];
+foreach ($currentTest as $questionText) {
+	/** @var Question $question */
+	$question = $allTests[$questionText];
 	$i++;   //číslo otázky
-	if ($answers['hasCorrectAnswer']) {
+	if ($question->hasCorrectAnswer()) {
+		$id = $question->correctAnswer->id;
+		$I->click("input[value=\"$id\"]");
 		continue;
 	}
-	foreach ($answers['answers'] as $answer) {
-		if (!$answer['tried']) {
-			$I->wantTo("click answer" . json_encode($answer));
+	foreach ($question->answers as $answer) {
+		if (!$answer->tried) {
 			//první nevyzkoušená otázka
 			$id = $answer['id'];
 //			$I->selectOption("input[name=a$i]", $id);
 			$I->click("input[value=\"$id\"]");
-			$answer['selected'] = true;
-			$answer['tried'] = true;
+			$question->selected = $answer;
+			$answer->tried = true;
 			break;
 		}
 	}
@@ -57,7 +114,18 @@ foreach ($currentTest as $question) {
 //submit formuláře
 $I->click('input[type=button]');
 
+$i = 0;
+foreach ($currentTest as $questionText) {
+	/** @var Question $question */
+	$question = $allTests[$questionText];
+	$i++;   //číslo otázky
+	$image = $I->grabTextFrom("~<img src=\"(.*?)\" ALT=\"status\"> (.*?) </td><td><b>$i.~");
+	if ($image == 'img/icon_good.jpg') {
+		$question['selected']['correct'] = true;
+	} else {
 
+	}
+}
 
 file_put_contents('tests.json', json_encode($allTests, JSON_PRETTY_PRINT));
 file_put_contents('testing.json', json_encode($array, JSON_PRETTY_PRINT));
